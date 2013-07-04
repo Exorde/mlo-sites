cd77b4935350b35f9453431840c8171bce062102
MusiqueLibre <dev@dogmazic.net>
2013-07-04 16:18:17 +0200

    commit before ftp push

--- forum/plugins/MloSsoAuthent/config.php
+++ forum/plugins/MloSsoAuthent/config.php
@@ -3,9 +3,9 @@
 // a no-brainer
 $db_conf = array(
             'host' => 'localhost',
-            'user' => '4834_users',
-            'pass' => '2_-!!(ezE)',
-            'db' => '4834_users',
+            'user' => '4836_user',
+            'pass' => 'axpdPUU2',
+            'db' => '4836_yii',
             'table' => 'users',
             'row' => 'session'
           );
